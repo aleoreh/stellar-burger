f6e5fcd78a399093bf59df41bec4c470e15fa608
Aleksey Orekhov <aleoreh@gmail.com>
2024-05-24 13:16:14 +0300

    Добавляет маршруты приложения

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,12 +1,97 @@
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  ForgotPassword,
+  Login,
+  NotFound404,
+  Profile,
+  ProfileOrders,
+  Register,
+  ResetPassword
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
-import { AppHeader } from '@components';
+import { AppHeader, IngredientDetails, Modal, OrderInfo } from '@components';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { ProtectedRoute } from '../protected-route/ProtectedRoute';
 
 const router = createBrowserRouter([
-  { path: '/', element: <ConstructorPage /> }
+  { path: '/', element: <ConstructorPage /> },
+  { path: '/feed', element: <Feed /> },
+  {
+    path: '/login',
+    element: (
+      <ProtectedRoute>
+        <Login />
+      </ProtectedRoute>
+    )
+  },
+  {
+    path: '/register',
+    element: (
+      <ProtectedRoute>
+        <Register />
+      </ProtectedRoute>
+    )
+  },
+  {
+    path: '/forgot-password',
+    element: (
+      <ProtectedRoute>
+        <ForgotPassword />
+      </ProtectedRoute>
+    )
+  },
+  {
+    path: '/reset-password',
+    element: (
+      <ProtectedRoute>
+        <ResetPassword />
+      </ProtectedRoute>
+    )
+  },
+  {
+    path: '/profile',
+    element: (
+      <ProtectedRoute>
+        <Profile />
+      </ProtectedRoute>
+    )
+  },
+  {
+    path: '/profile/orders',
+    element: (
+      <ProtectedRoute>
+        <ProfileOrders />
+      </ProtectedRoute>
+    )
+  },
+  {
+    path: '/feed/:number',
+    element: (
+      <Modal title='OrderInfo' onClose={() => {}}>
+        <OrderInfo />
+      </Modal>
+    )
+  },
+  {
+    path: '/ingredients/:id',
+    element: (
+      <Modal title='IngredientsDetails' onClose={() => {}}>
+        <IngredientDetails />
+      </Modal>
+    )
+  },
+  {
+    path: '/profile/orders/:number',
+    element: (
+      <Modal title='OrderInfo' onClose={() => {}}>
+        <OrderInfo />
+      </Modal>
+    )
+  },
+  { path: '/*', element: <NotFound404 /> }
 ]);
 
 const App = () => (
