c675f05b55f22772fafc84b6c3e87feca058c520
Aleksey Orekhov <aleoreh@gmail.com>
2024-05-26 22:24:00 +0300

    Добавляет булку при нажатии на ингредиент

--- src/components/burger-ingredient/burger-ingredient.tsx
+++ src/components/burger-ingredient/burger-ingredient.tsx
@@ -3,12 +3,19 @@ import { useLocation } from 'react-router-dom';
 
 import { BurgerIngredientUI } from '@ui';
 import { TBurgerIngredientProps } from './type';
+import { useDispatch } from '../../app/store';
+import burgerDepot from '../../services/slices/burgerSlice';
 
 export const BurgerIngredient: FC<TBurgerIngredientProps> = memo(
   ({ ingredient, count }) => {
     const location = useLocation();
+    const dispatch = useDispatch();
 
-    const handleAdd = () => {};
+    const handleAdd = () => {
+      if (ingredient.type === 'bun') {
+        dispatch(burgerDepot.putBun(ingredient));
+      }
+    };
 
     return (
       <BurgerIngredientUI
