48c219974fd7ce8ad1909190ee496b29ec1d62c8
Aleksey Orekhov <aleoreh@gmail.com>
2024-05-27 15:36:05 +0300

    Создаёт модуль validationHooks

---
+++ src/hooks/validationHooks.ts
@@ -0,0 +1,79 @@
+import { useState } from 'react';
+
+type ValidatedField<N> = {
+  name: N;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  error: boolean;
+  errorText: string;
+  'data-initial': boolean;
+};
+
+type Validate = (value: string) => boolean;
+
+export type Validator = { validate: Validate; message: string };
+
+const createValidator = (validate: Validate, message: string): Validator => ({
+  validate,
+  message
+});
+
+// ~~~~~~~~~~~~~~ validators ~~~~~~~~~~~~~ //
+
+const email: Validator = createValidator(
+  (value: string) =>
+    /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(value),
+  'Некорректный адрес электронной почты'
+);
+
+const nonEmptyString: Validator = createValidator(
+  (value: string) => value.length > 0,
+  'Поле должно быть заполнено'
+);
+
+const password: Validator = createValidator(
+  (value) => value.length > 1,
+  'Слишком короткий пароль'
+);
+
+// ~~~~~~~~~~~~~~~~ hooks ~~~~~~~~~~~~~~~~ //
+
+export const useValidatedField = <N extends string>(
+  name: N,
+  value: string,
+  setValue: (value: string) => void,
+  validator: Validator
+): ValidatedField<N> => {
+  const [isInitial, setIsInitial] = useState(true);
+  const [error, setError] = useState<string | undefined>(undefined);
+  return {
+    name,
+    value,
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => {
+      setIsInitial(false);
+      if (validator.validate(e.target.value)) {
+        setError(undefined);
+      } else {
+        setError(validator.message);
+      }
+      setValue(e.target.value);
+    },
+    error: error !== undefined,
+    errorText: error || '',
+    'data-initial': isInitial
+  };
+};
+
+export const useFormValidation = <T, N>(fields: ValidatedField<keyof N>[]) => ({
+  isValid: fields.every((value) => !value['data-initial'] && !value.error)
+});
+
+// ~~~~~~~~~~~~~~~ exports ~~~~~~~~~~~~~~~ //
+
+export const validators = {
+  email,
+  nonEmptyString,
+  password
+};
+
+export default validators;
