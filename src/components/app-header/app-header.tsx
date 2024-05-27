6af33a6b420c8f7fa14a21add33b39648926ff73
Aleksey Orekhov <aleoreh@gmail.com>
2024-05-27 20:33:38 +0300

    Отображает имя аутентифицированного пользователя

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,4 +1,10 @@
 import { FC } from 'react';
 import { AppHeaderUI } from '@ui';
+import { useSelector } from '../../app/store';
+import authDepot from '../../services/slices/authSlice';
 
-export const AppHeader: FC = () => <AppHeaderUI userName='' />;
+export const AppHeader: FC = () => {
+  const auth = useSelector(authDepot.selectAuth);
+
+  return <AppHeaderUI userName={auth.user?.name} />;
+};
