da4906c69cb778fdf13d8c46495cbf5e3332aa54
Aleksey Orekhov <aleoreh@gmail.com>
2024-05-27 20:44:39 +0300

    Добавляет ссылку на домашнюю страницу

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -1,19 +1,22 @@
-import React, { FC } from 'react';
-import styles from './app-header.module.css';
-import { TAppHeaderUIProps } from './type';
 import {
   BurgerIcon,
   ListIcon,
   Logo,
   ProfileIcon
 } from '@zlden/react-developer-burger-ui-components';
+import { FC } from 'react';
+import { Link } from 'react-router-dom';
+import styles from './app-header.module.css';
+import { TAppHeaderUIProps } from './type';
 
 export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
   <header className={styles.header}>
     <nav className={`${styles.menu} p-4`}>
       <div className={styles.menu_part_left}>
         <>
-          <BurgerIcon type={'primary'} />
+          <Link to='/'>
+            <BurgerIcon type={'primary'} />
+          </Link>
           <p className='text text_type_main-default ml-2 mr-10'>Конструктор</p>
         </>
         <>
