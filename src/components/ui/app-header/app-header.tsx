4d8c842e96f35bf1d9fab238c9dfcd065b06c96e
Aleksey Orekhov <aleoreh@gmail.com>
2024-05-29 22:56:57 +0300

    Выделяет активный маршрут

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -4,8 +4,9 @@ import {
   Logo,
   ProfileIcon
 } from '@zlden/react-developer-burger-ui-components';
+import clsx from 'clsx';
 import { FC } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './app-header.module.css';
 import { TAppHeaderUIProps } from './type';
 
@@ -13,29 +14,40 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
   <header className={styles.header}>
     <nav className={`${styles.menu} p-4`}>
       <div className={styles.menu_part_left}>
-        <>
-          <Link to='/'>
-            <BurgerIcon type={'primary'} />
-          </Link>
+        <NavLink
+          to='/'
+          className={({ isActive }) =>
+            clsx(styles.link, isActive && styles.link_active)
+          }
+        >
+          <BurgerIcon type={'primary'} />
           <p className='text text_type_main-default ml-2 mr-10'>Конструктор</p>
-        </>
-        <>
-          <Link to='/feed'>
-            <ListIcon type={'primary'} />
-          </Link>
+        </NavLink>
+        <NavLink
+          to='/feed'
+          className={({ isActive }) =>
+            clsx(styles.link, isActive && styles.link_active)
+          }
+        >
+          <ListIcon type={'primary'} />
           <p className='text text_type_main-default ml-2'>Лента заказов</p>
-        </>
+        </NavLink>
       </div>
       <div className={styles.logo}>
         <Logo className='' />
       </div>
       <div className={styles.link_position_last}>
-        <Link to='/profile'>
+        <NavLink
+          to='/profile'
+          className={({ isActive }) =>
+            clsx(styles.link, isActive && styles.link_active)
+          }
+        >
           <ProfileIcon type={'primary'} />
-        </Link>
-        <p className='text text_type_main-default ml-2'>
-          {userName || 'Личный кабинет'}
-        </p>
+          <p className='text text_type_main-default ml-2'>
+            {userName || 'Личный кабинет'}
+          </p>
+        </NavLink>
       </div>
     </nav>
   </header>
