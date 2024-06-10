aee16f18a9c72c8b782581e85fa339501c00ec67
Aleksey Orekhov <aleoreh@gmail.com>
2024-06-10 16:01:44 +0300

    Настраивает переменные окружения Cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,16 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default defineConfig({
+  env: {
+    BURGER_API_URL: process.env.BURGER_API_URL
+  },
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-  },
+    baseUrl: 'http://localhost:4000'
+  }
 });
