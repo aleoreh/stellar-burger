ebcfa58b95df5b4af10077daf10295d1353ce03f
Aleksey Orekhov <aleoreh@gmail.com>
2024-06-11 16:06:10 +0300

    WIP: тестирование отправки заказа

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -1,8 +1,18 @@
-const fixture = 'ingredients.json';
+const ingredientsFixture = 'ingredients.json';
+const orderAnswer = 'order_answer.json';
 
 beforeEach(() => {
   cy.intercept('GET', `${Cypress.env('BURGER_API_URL')}/ingredients`, {
-    fixture
+    fixture: ingredientsFixture
+  });
+
+  // /api/orders/all
+
+  // post https://norma.nomoreparties.space/api/orders
+  // {"ingredients":["643d69a5c3f7b9001cfa093c","643d69a5c3f7b9001cfa0941","643d69a5c3f7b9001cfa093c"]}
+  // {"success":true,"name":"Краторный био-марсианский бургер","order":{"number":3845}}
+  cy.intercept('POST', `${Cypress.env('BURGER_API_URL')}/orders`, {
+    fixture: orderAnswer
   });
 });
 
@@ -80,9 +90,11 @@ describe('Создание заказа', () => {
   before(() => {
     cy.setCookie('accessToken', 'accessToken');
   });
+
   after(() => {
     cy.clearCookies();
   });
+
   it('правильно создаёт заказ', () => {
     cy.visit('/', {
       onBeforeLoad(win) {
@@ -95,6 +107,43 @@ describe('Создание заказа', () => {
           })
         );
       }
+    });
+
+    cy.fixture('ingredients').then(({ data }) => {
+      const buns = data.filter(({ type }) => type === 'bun');
+      const mains = data.filter(({ type }) => type === 'main');
+
+      // находим раздел с ингредиентами
+      const ingredientsCategoryCy = cy.get(
+        '*[data-cy="ingredients-category-title"]'
+      );
+
+      // щёлкаем по булке и одной начинке
+      ingredientsCategoryCy
+        .get('*[data-cy="burger-ingredient"]')
+        .filter(`:contains("${buns[0].name}")`)
+        .contains('Добавить')
+        .click();
+
+      ingredientsCategoryCy
+        .get('*[data-cy="burger-ingredient"]')
+        .filter(`:contains("${mains[0].name}")`)
+        .contains('Добавить')
+        .click();
+
+      // находим раздел с составом заказа
+      const constructorCy = cy.get('*[data-cy="burger-constructor"]');
+
+      // щёлкаем на оформление заказа
+      constructorCy.contains('Оформить заказ').click();
+
+      // находим модальное окно с отправленным заказом
+      const modalCy = cy.contains('идентификатор заказа').parent();
+
+      // проверяем, что оно содержит номер заказа
+      cy.fixture('order_answer').then(({ order }) => {
+        modalCy.should('contain', `${order.number}`);
+      });
     });
 
     cy.clearLocalStorage();
