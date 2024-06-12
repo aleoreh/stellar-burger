3a65b2f6c44076cae81706fcf6450d404b3607a5
Aleksey Orekhov <aleoreh@gmail.com>
2024-06-12 13:55:58 +0300

    Jest: + добавление булочки

--- src/app/store.spec.ts
+++ src/app/store.spec.ts
@@ -1,5 +1,21 @@
 import { PayloadAction, createAction } from '@reduxjs/toolkit';
+import orderDepot from '../services/slices/orderSlice';
+import { TIngredient } from '../utils/types';
 import store, { RootState } from './store';
+
+const mockBun: TIngredient = {
+  _id: '643d69a5c3f7b9001cfa093c',
+  name: 'TEST! Краторная булка N-200i',
+  type: 'bun',
+  proteins: 80,
+  fat: 24,
+  carbohydrates: 53,
+  calories: 420,
+  price: 1255,
+  image: 'https://code.s3.yandex.net/react/code/bun-02.png',
+  image_mobile: 'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
+  image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png'
+};
 
 const dispatchAction = <T>(
   action: PayloadAction<T>,
@@ -19,5 +35,16 @@ describe('store', () => {
     );
 
     expect(currentState).toEqual(initialState);
+  });
+});
+
+describe('Редуктор среза "order"', () => {
+  it('может подставлять булочку', () => {
+    const { currentState } = dispatchAction(orderDepot.putBun(mockBun));
+
+    const currentBun = orderDepot.selectBun(currentState);
+
+    expect(currentBun).toBeTruthy();
+    expect(currentBun).toEqual(mockBun);
   });
 });
