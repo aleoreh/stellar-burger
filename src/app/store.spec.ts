b79f6feb5a231054c849359db5a059f238877319
Aleksey Orekhov <aleoreh@gmail.com>
2024-06-12 14:02:09 +0300

    Jest: + добавление ингредиента

--- src/app/store.spec.ts
+++ src/app/store.spec.ts
@@ -17,6 +17,20 @@ const mockBun: TIngredient = {
   image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png'
 };
 
+const mockIngredient: TIngredient = {
+  _id: '643d69a5c3f7b9001cfa0941',
+  name: 'Биокотлета из марсианской Магнолии',
+  type: 'main',
+  proteins: 420,
+  fat: 142,
+  carbohydrates: 242,
+  calories: 4242,
+  price: 424,
+  image: 'https://code.s3.yandex.net/react/code/meat-01.png',
+  image_mobile: 'https://code.s3.yandex.net/react/code/meat-01-mobile.png',
+  image_large: 'https://code.s3.yandex.net/react/code/meat-01-large.png'
+};
+
 const dispatchAction = <T>(
   action: PayloadAction<T>,
   initialState?: RootState
@@ -46,5 +60,17 @@ describe('Редуктор среза "order"', () => {
 
     expect(currentBun).toBeTruthy();
     expect(currentBun).toEqual(mockBun);
+  });
+
+  it('может добавлять ингредиент', () => {
+    const { currentState } = dispatchAction(
+      orderDepot.addIngredient(mockIngredient)
+    );
+
+    const currentIngredients = orderDepot.selectIngredients(currentState);
+
+    expect(currentIngredients.length).toBe(1);
+
+    expect(currentIngredients[0]._id).toEqual(mockIngredient._id);
   });
 });
